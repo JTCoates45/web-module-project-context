a5db9ebbd843d6108e7574946cf5f3abc75251fe
JT Coates <wyosred@yahoo.com>
2022-05-19 23:50:36 -0600

    Fixed and export

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import ProviderContext from '../Context/ProductContext'
+
 
 // Components
 import Product from './Product';
 
+
 const Products = props => {
+	const {products} = useContext(ProviderContext);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
 				/>
 			))}
 		</div>
